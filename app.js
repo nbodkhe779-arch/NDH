0ed720344952a487a8e744823c77f72b4d575143
nbodkhe779-arch <nbodkhe779@gmail.com>
2026-04-30 04:55:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,17 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAcElCId4770Q7vW70WfmG8m0CTF__KJbY",
-  authDomain: "nicks-design-hube.firebaseapp.com",
-  projectId: "nicks-design-hube",
-  storageBucket: "nicks-design-hube.appspot.com",
-  messagingSenderId: "361289302201",
-  appId: "1:361289302201:web:e238156e92777c39e5db68"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-
-/* SIGNUP */
 window.signup = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  auth.createUserWithEmailAndPassword(email, password)
+  firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(() => alert("Signup Success"))
     .catch(e => alert(e.message));
 };
 
-/* LOGIN */
 window.login = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  auth.signInWithEmailAndPassword(email, password)
+  firebase.auth().signInWithEmailAndPassword(email, password)
     .then(() => alert("Login Success"))
     .catch(e => alert(e.message));
-};
-
-/* LOGOUT */
-window.logout = function () {
-  auth.signOut();
-  alert("Logged out");
 };
