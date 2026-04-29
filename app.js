a976dd3516f42553cf377ffe89a7e3e337c02045
nbodkhe779-arch <nbodkhe779@gmail.com>
2026-04-30 04:58:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ window.login = function () {
     .then(() => alert("Login Success"))
     .catch(e => alert(e.message));
 };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAcElCId4770Q7wV70WfmG8m0CTF__KJbY",
+  authDomain: "nicks-design-hube.firebaseapp.com",
+  projectId: "nicks-design-hube",
+  storageBucket: "nicks-design-hube.firebasestorage.app",
+  messagingSenderId: "361289302201",
+  appId: "1:361289302201:web:e238156e92777c39e5db68",
+  measurementId: "G-HTTZ5ZSV78"
+};
