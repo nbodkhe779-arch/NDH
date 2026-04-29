98c6f877466f9d29553dfadae052715020dc0f5a
nbodkhe779-arch <nbodkhe779@gmail.com>
2026-04-30 04:32:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyAcElCId4770Q7vW70WfmG8m0CTF__KJbY",
+  authDomain: "nicks-design-hube.firebaseapp.com",
+  projectId: "nicks-design-hube",
+  storageBucket: "nicks-design-hube.appspot.com",
+  messagingSenderId: "361289302201",
+  appId: "1:361289302201:web:e238156e92777c39e5db68"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 
+/* SIGNUP */
 window.signup = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -15,6 +21,7 @@ window.signup = function () {
     .catch(e => alert(e.message));
 };
 
+/* LOGIN */
 window.login = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
