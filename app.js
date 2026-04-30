bef5a248fcd3d069059caa494f580f0c075f2e3f
nbodkhe779-arch <nbodkhe779@gmail.com>
2026-04-30 06:25:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -68,3 +68,33 @@ firebase.firestore().collection("services")
 
     document.getElementById("servicesList").innerHTML = html;
   });
+window.addService = function () {
+
+  const file = document.getElementById("imageFile").files[0];
+  const title = document.getElementById("title").value;
+  const price = document.getElementById("price").value;
+
+  if (!file) {
+    alert("Select image");
+    return;
+  }
+
+  const storageRef = firebase.storage().ref("services/" + file.name);
+
+  storageRef.put(file).then(() => {
+
+    storageRef.getDownloadURL().then((url) => {
+
+      firebase.firestore().collection("services").add({
+        title: title,
+        price: price,
+        image: url
+      });
+
+      alert("Service Uploaded ✔");
+
+    });
+
+  });
+
+};
